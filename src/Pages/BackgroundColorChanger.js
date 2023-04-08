13acfe724ebd7ab0a3c47cbd01307e6e1de4c02a
rusty-gahoi <vipulrocks28@gmail.com>
2023-04-08 20:17:40 +0530

    final code

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 
 export const BackgroundColorChanger = () =>{
+    let { colorname } = useParams();
     return(
+        <main style={{height: "100vh", backgroundColor: `${colorname}`}}>
+            Background Color Changer
+        </main>
        
     )
 }
